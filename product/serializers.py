57e68e64881e82a5f0327c9b06ca42ba7dac4fc0
Doolotbek-Zaparbekov <victusdz2008@gmail.com>
2025-07-21 14:50:15 +0600

    HW 4

--- product/serializers.py
+++ product/serializers.py
@@ -1,33 +1,46 @@
 from rest_framework import serializers
 from .models import Category, Product, Review
 
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ['id', 'text', 'stars']
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-class ProductWithReviewsSerializer(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True, read_only=True)
-    rating = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Product
-        fields = ['id', 'title', 'description', 'price', 'category', 'reviews', 'rating']
-
-    def get_rating(self, obj):
-        reviews = obj.reviews.all()
-        if reviews.exists():
-            return round(sum([review.stars for review in reviews]) / reviews.count(), 2)
-        return None
-
 class CategorySerializer(serializers.ModelSerializer):
-    products_count = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=100)
 
     class Meta:
         model = Category
         fields = ['id', 'name', 'products_count']
+        read_only_fields = ['products_count']
+
+    def validate_name(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Имя категории не может быть пустым.")
+        return value
+
+class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=100)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'price', 'category']
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Название продукта не может быть пустым.")
+        return value
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Цена должна быть больше 0.")
+        return value
+
+class ReviewSerializer(serializers.ModelSerializer):
+    text = serializers.CharField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
+    class Meta:
+        model = Review
+        fields = ['id', 'text', 'stars', 'product']
+
+    def validate_text(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Текст отзыва не может быть пустым.")
+        return value
